cf44c805f86bb75164645a7633adc8b7ce015b1a
Thomas K <tk1987@mailbox.org>
2022-02-17 16:45:25 +0100

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,72 +1,38 @@
 #!/bin/bash
 
-if [ "$EUID" != 0 ];then SU=sudo;fi
+# Wenn Fehler auftritt, Skript beenden
+set -e
 
-mainmenu () {
-  local SEL=$( whiptail --menu "Hauptmenü" 0 0 0 --ok-button Ok --cancel-button Beenden \
-    1. "Aktualisieren / Pakete installieren" \
-    2. "Computerkonfiguration" \
-    3. "Benutzerkonfiguration" \
-    4. "Fremdsystem reparieren (chroot)" \
-    3>&1 1>&2 2>&3
-  )
-  case ${SEL:0:1} in 1) update;; 2) cconfig;; 3) uconfig;; 4) chrootfs;; esac
-}
+# Resume Variable für Swap setzen
+lsblk -lno uuid,fstype |awk '$2 ~ "^swap" {print "RESUME=UUID="$1}'| sudo tee /etc/initramfs-tools/conf.d/resume
 
-update () {
-  aptls=$( whiptail --checklist "Pakete installieren: " 0 0 0 --ok-button Start --cancel-button Abbruch \
-    "gparted"              "Paritionierungswerkzeug"                           on  \
-    "samba"                "Netzwerkfreigaben "                                on  \
-    "p7zip-full"           "Paketverwaltung"                                   on  \
-    "oxygen-icon-theme"    "Symbolthema"                                       on  \
-    "breeze-gtk-theme"     "Fensterthema"                                      on  \
-    "console-data"         "Sprachpakete für Konsole"                          on  \
-    "keepassxc"            "Passwortmanager"                                   on  \
-    "qemu-user-static"     "Chroot auf in anderen Architekturen ermöglichen"   off \
-    "git"                  "Github verwaltung"                                 off \
-    "openssh-server"       "Fernverwaltungsserver "                            off \
-    "imagemagick"          "Bildbearbeitungs-Konsolen-Tools"                   off \
-    "xrdp"                 "Remotedesktop-Server"                              off \
-    "rdesktop"             "Remotedesktop-Client"                              off \
-    3>&1 1>&2 2>&3 | xargs
-  )
-  if [ ! -z "$aptls" ];then
-    $SU apt update
-    $SU apt full-upgrade -y $aptls
-  fi
-  mainmenu
-}
+# Apport deaktivieren
+sudo sed -iE 's#^(enabled=).*$#\10#' /etc/default/apport
 
-cconfig () {
-  cconf=$( whiptail --checklist "Computerkonfiguration " 0 0 0 --ok-button Ok --cancel-button Abbruch \
-    "upgrade"              "Aktualisierungsskript hinzufügen"                  on  \
-    "autoupdate"           "Automatische updates per Skript aktivieren"        on  \
-    "noapport"             "Apport deaktivieren"                               on  \
-    "supower"              "Sudoerseintrag für Gruppe Users anlegen"           on  \
-    "console2german"       "Deutsche Sprache auf Konsole einstellen"           on  \
-    3>&1 1>&2 2>&3 | xargs
-  )
-  mainmenu
-}
+# Pakete installieren
+if [ ! -f "apt.lst" ];then echo -e "\e[31mPaketliste wurde nicht gefunden\e[0m";exit 1;fi
+(sudo apt update && cat apt.lst |xargs sudo apt-get upgrade -y) &
+PID=$!
 
-uconfig () {
-  uconf=$( whiptail --checklist "Benutzerkonfiguration " 0 0 0 --ok-button Ok --cancel-button Abbruch \
-    "add2users"            "Benutzer zu users hinzufügen"                 on  \
-    "add2samba"            "Benutzer zu Gruppe Sambashare hinzufügen"     on  \
-    "oxygen"               "Oxygen als Symbolthema einstellen"            on  \
-    "breeze"               "Breeze-Dark als Fensterthema einstellen"      on  \
-    "autologin"            "Benutzer automatisch in GUI einloggen"        off \
-    "ttyautologin"         "Benutzer automatisch in Konsole einloggen"    off \
-    3>&1 1>&2 2>&3 | xargs
-  )
-  mainmenu
-}
+# Panel-Theme setzen
+gsettings set org.mate.panel default-layout 'ubuntu-mate'
+mate-panel --reset
 
-chrootfs () {
-  local IFS=$'\n'
-  local DEV=$(lsblk -lno fstype,name,size,mountpoint |grep ^ext.|awk '$4 ~ "/(media|mnt)|^$" {printf "/dev/%-20s\n%8s\n",$2,$3}')
-  local SEL=$( whiptail --menu "Gerät für Chroot auswählen: " 0 0 0 --ok-button Ok --cancel-button Abbruch ${DEV[@]} 3>&1 1>&2 2>&3 )
-  echo $SEL
-}
+# Hintergrund setzen
+for PIC in $(find /usr/share/backgrounds/|grep andrew);do
+  gsettings set org.mate.background picture-filename "$PIC"
+done
 
-mainmenu
+# GTK-Theme setzen
+for THEME in $(ls -d /usr/share/themes/*|grep -ioP "blackmate");do
+  gsettings set org.mate.interface gtk-theme "$GTK"
+done
+
+# Fenster-Theme setzen
+for THEME in $(ls -d /usr/share/themes/*|grep -ioP "bluementa");do
+  gsettings set org.mate.Marco.general theme "$THEME"
+fi
+
+while ps -p $PID >/dev/null; do
+  sleep 10
+done
